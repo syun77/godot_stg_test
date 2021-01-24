2a24da829b8135de0d01bc9e07922e0cf2baf9e1
syun <2dgames.jp@gmail.com>
2021-01-24 09:51:47 +0900

    敵の消滅処理を実装

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,27 +1,31 @@
-# 物理シュミレートしない物理挙動を持つオブジェクト
 extends KinematicBody2D
 
-# 移動方向
+# 体力
+var hp = 5
+
+# 移動量
 var velocity = Vector2()
 
-# 開始
 func start(x, y, deg, speed):
 	position = Vector2(x, y)
 	velocity.x = speed * cos(deg2rad(deg))
 	velocity.y = speed * -sin(deg2rad(deg))
 
-func _ready():
-	pass
-
-func _physics_process(delta):
-	if Global.isInScreen(self) == false:
-		# 画面外に出たので削除
+func hit(damage):
+	# ダメージ処理
+	hp -= damage
+	if hp <= 0:
+		# 消滅
 		queue_free()
 
-	# 移動と衝突判定を行う
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	
+	if Global.isInScreen(self) == false:
+		queue_free()
+	
+	# 当たり判定
 	var col = move_and_collide(velocity * delta)
-	if col:
-		if col.collider.name == "Enemy":
-			# 敵と衝突したので削除
-			col.collider.hit(1) # 敵に1ダメージ
-			queue_free()
+	# TODO: 未実装
